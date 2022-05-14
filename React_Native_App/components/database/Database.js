7ea88348b2a04f6bf802ea04401dd13b4145ec61
Pranay <rathipranay27@gmail.com>
2022-05-14 19:27:46 +0530

    Added more data to database

--- React_Native_App/components/database/Database.js
+++ React_Native_App/components/database/Database.js
@@ -109,4 +109,130 @@ export const Items = [
       require('../database/images/accessories/boultairbass3.png'),
     ],
   },
+  {
+    id: 7,
+    category: 'Vegetable',
+    productName: 'Brinjal',
+    productPrice: 100,
+    offPercentage: 10,
+    description:
+      'Most commonly purple, the spongy, absorbent fruit is used in several cuisines. Typically used as a vegetable in cooking,.',
+    isOff: true,
+    productImage: require('../database/images/Vegetable/ilbrinjal.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/Vegetable/ilbrinjal.png')
+    ],
+  },
+  {
+    id: 8,
+    category: 'Vegetable',
+    productName: 'Brokoli',
+    productPrice: 100,
+    offPercentage: 10,
+    description:
+      'Boiling substantially reduces the levels of broccoli glucosinolates, while other cooking methods, such as steaming, microwaving, and stir frying, have no significant effect on glucosinolate levels.',
+    isOff: true,
+    productImage: require('../database/images/Vegetable/il_brokoli.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/Vegetable/il_brokoli.png')
+    ],
+  },
+  {
+    id: 9,
+    category: 'Vegetable',
+    productName: 'Cauliflower',
+    productPrice: 100,
+    offPercentage: 10,
+    description:
+      'When cauliflower is mature, heads appear as clear white, compact, and 15–20 cm (6–8 in) in diameter, and should be cooled shortly after harvest.Forced air cooling to remove heat from the field during hot weather may be needed for optimal preservation. Short-term storage is possible using cool, high-humidity storage conditions.',
+    isOff: true,
+    productImage: require('../database/images/Vegetable/il_cauliflawer.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/Vegetable/il_cauliflawer.png')
+    ],
+  },
+  {
+    id: 10,
+    category: 'Vegetable',
+    productName: 'Potato',
+    productPrice: 100,
+    offPercentage: 10,
+    description:
+      'The potato is a starchy tuber of the plant Solanum tuberosum and is a root vegetable native to the Americas. The plant is a perennial in the nightshade family Solanaceae. Potato cultivars appear in a variety of colors, shapes, and sizes.',
+    isOff: true,
+    productImage: require('../database/images/Vegetable/il_potato.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/Vegetable/il_potato.png')
+    ],
+  },
+  {
+    id: 11,
+    category: 'Vegetable',
+    productName: 'Tomato',
+    productPrice: 100,
+    offPercentage: 10,
+    description:
+      'Tomatoes are usually red, scarlet, or yellow, though green and purple varieties do exist, and they vary in shape from almost spherical to oval and elongate to pear-shaped. Each fruit contains at least two cells of small seeds surrounded by jellylike pulp.',
+    isOff: true,
+    productImage: require('../database/images/Vegetable/il_tomato.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/Vegetable/il_tomato.png')
+    ],
+  },
+  {
+    id: 12,
+    category: 'Laptop',
+    productName: 'ASUS ROG Zephyrus S Ultra Slim Gaming Laptop',
+    productPrice: 198000,
+    offPercentage: 10,
+    description:
+      'ASUS ROG Zephyrus S Ultra Slim Gaming Laptop, 15.6" 144Hz IPS-Type Full HD, GeForce RTX 2080, Intel Core i7-8750H CPU, 16GB DDR4, 512GB PCIe Nvme SSD, Aura Sync RGB, Windows 10 Pro - GX531GX-XS74',
+    isOff: true,
+    productImage: require('../database/images/Laptop/ASUS_ROG_1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/Laptop/ASUS_ROG_1.png'),
+      require('../database/images/Laptop/ASUS_ROG_2.png'),
+      require('../database/images/Laptop/ASUS_ROG_3.png'),
+    ],
+  },
+  {
+    id: 13,
+    category: 'Laptop',
+    productName: 'Apple MacBook Pro',
+    productPrice: 127000,
+    offPercentage: 10,
+    description:
+      '2020 Apple MacBook Pro (13.3-inch/33.78 cm, Apple M1 chip with 8‑core CPU and 8‑core GPU, 8GB RAM, 512GB SSD) - Silver',
+    isOff: true,
+    productImage: require('../database/images/Laptop/Apple_MacBook_1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/Laptop/Apple_MacBook_1.png'),
+      require('../database/images/Laptop/Apple_MacBook_2.png'),
+      require('../database/images/Laptop/Apple_MacBook_3.png'),
+    ],
+  },
+  {
+    id: 14,
+    category: 'Laptop',
+    productName: 'Lenovo Legion 7 ',
+    productPrice: 99999,
+    offPercentage: 10,
+    description:
+      'Lenovo Legion 7 AMD Ryzen 9 5900HX 16 inches QHD IPS 500Nits Gaming Laptop (32GB/1TB SSD/Windows 10/MS Office/NVIDIA RTX 3080 16GB/165Hz Refresh Rate/RGB Backlit Keyboard/Storm Grey/2.5Kg), 82N6008CIN',
+    isOff: true,
+    productImage: require('../database/images/Laptop/Lenovo_Legion_1.png'),
+    isAvailable: true,
+    productImageList: [
+      require('../database/images/Laptop/Lenovo_Legion_1.png'),
+      require('../database/images/Laptop/Lenovo_Legion_2.png'),
+      require('../database/images/Laptop/Lenovo_Legion_3.png'),
+    ],
+  }
 ];
